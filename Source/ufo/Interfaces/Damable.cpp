67735b6a072bfb632cb1a989231f770fd4004026
andrescrd <c.andresricardo@gmail.com>
2020-05-04 21:57:41 -0300

    adding intrefaces

---
+++ Source/ufo/Interfaces/Damable.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Damable.h"
+
+// Add default functionality here for any IDamable functions that are not pure virtual.
